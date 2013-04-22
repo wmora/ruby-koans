d586c15d9333a16cfcc58c4dafec3796d296d568
William Mora <wilmor24@gmail.com>
2013-04-21 20:48:17 -0430

    Triangles

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if (a == b) && (b == c) 
+  	return :equilateral
+  elsif (a != b) && (b != c) && (a != c) 
+  	return :scalene
+  else 
+  	return :isosceles  
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
